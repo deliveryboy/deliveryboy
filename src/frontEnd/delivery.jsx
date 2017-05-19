989f9c39b1b64e0be23261d356df6e0d407727fd
abeeralshaer <abber2000g@gmail.com>
2017-05-19 11:00:09 -0500

    add three jsx file for the three bundles

---
+++ src/frontEnd/delivery.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import store from './store.js';
+import App from './components/containers/App.js';
+
+const Main = () => {
+  return(
+  <Provider store={store}>
+    <div>
+      <App/>
+    </div>
+  </Provider>
+  );
+};
+
+reactDOM.render(
+  <Main />, document.getElementById('myApp'));
