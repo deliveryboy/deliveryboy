c91108c912768e5fb10b6af841afabb5e72196a8
abeeralshaer <abber2000g@gmail.com>
2017-05-19 14:10:18 -0500

    add getuser action

--- src/frontEnd/actions.js
+++ src/frontEnd/actions.js
@@ -1,7 +1,36 @@
+import store from './store.js';
+
 const ChangeCurrentView = (currentView) => {
   return {type: 'CHANGE_CURRENT_VIEW', payload: currentView};
 };
 
-export {
-  ChangeCurrentView
+const getUser = (user) => {
+  fetchWrapper('/' + user).then((res) => {
+    if (res != null) {
+
+      store.dispatch({type: 'UPDATE_PROFILE', payload: res});
+    }
+
+  });
+
 };
+const fetchWrapper = (url, method, body) => {
+  return fetch(url, {
+    method: method || 'GET',
+    body: JSON.stringify(body),
+    credentials: 'include',
+    headers: {
+      'Accept': 'application/json',
+      'content-type': 'application/json'
+    }
+  }).then(res => {
+    if (res.status === 200) {
+      return res.json();
+    } else {
+      return null;
+    }
+  });
+
+};
+
+export {ChangeCurrentView,getUser};
