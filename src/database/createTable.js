9bcbaa466989c79ca3105ac69e76b458a3dd4325
shahenaz Monia <shahy.m.93@gmail.com>
2017-05-19 10:39:28 +0300

    createtable file

---
+++ src/database/createTable.js
@@ -0,0 +1,41 @@
+const async = require('async');
+const pool = require('./pool.js');
+
+module.exports = (cb) => {
+  pool.connect((err, client, done) => {
+    if (err) {
+      return cb(err);
+    }
+    async.series([
+      (callback) => {
+        client.query(`CREATE TABLE IF NOT EXISTS customers (
+           id SERIAL PRIMARY KEY,
+
+        )`,callback);
+      },
+      (callback) => {
+        client.query(`CREATE TABLE IF NOT EXISTS resturants (
+           id SERIAL PRIMARY KEY,
+        )`,callback);
+      },
+      (callback) => {
+        client.query(`CREATE TABLE IF NOT EXISTS meals (
+           id SERIAL PRIMARY KEY,
+        )`,callback);
+      },
+      (callback) => {
+        client.query(`CREATE TABLE IF NOT EXISTS deliveryboy (
+           id SERIAL PRIMARY KEY,
+        )`,callback);
+      },
+      (callback) => {
+        client.query(`CREATE TABLE IF NOT EXISTS orders (
+           id SERIAL PRIMARY KEY,
+        )`,callback);
+      }
+    ], (err) => {
+      done(err);
+      return cb(err);
+    });
+  });
+};
