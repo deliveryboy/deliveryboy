5f0b07856fb32aeef233c9a31a01a0a4ddc4f499
Muhammed Mushtaha <mushtaham@filistin.co>
2017-05-19 16:40:08 +0300

    deleted files after fixing front split

--- src/fronEnd/reducer.js
+++
@@ -1,9 +0,0 @@
-const currentView = (state='',action)=>{
-  switch(action.type){
-  case 'CHANGE_CURRENT_VIEW':
-    return action.payload;
-  default:
-    return state;
-  }
-};
-export {currentView};
