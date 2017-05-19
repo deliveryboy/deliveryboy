0f42b5a384155fa8ff7384d63dce6f429cfe9302
shahenaz Monia <shahy.m.93@gmail.com>
2017-05-19 18:36:23 +0300

    add restaurant routes

--- src/backEnd/routes.js
+++ src/backEnd/routes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const customersControllers = require('./controllers/customersControllers.js');
 const restaurantsControllers = require('./controllers/restaurantsControllers.js')
 module.exports = router
-.get('/', customersControllers.getCustomers)
+.get('/',customersControllers.getHomePage)
+.get('/customers', customersControllers.getCustomers)
 .get('/restaurants',restaurantsControllers.getRestaurants);
