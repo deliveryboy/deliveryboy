44cdab1498561db05430ed8557d2f739d63ea41d
Muhammed Mushtaha <mushtaham@filistin.co>
2017-05-20 01:49:54 +0300

    fix map func

--- src/frontEnd/components/Resturant.jsx
+++ src/frontEnd/components/Resturant.jsx
@@ -6,7 +6,7 @@ const Resturants = ({restaurants}) => {
     <div className='row'>
       {restaurants.map(function(restaurant) {
         return (
-          <div className="col-xs-12 col-sm-6 col-md-3 float-card">
+          <div className="col-xs-12 col-sm-6 col-md-6 float-card">
             <div className="card">
               <img className="card-img-top img-responsive" src='http://placehold.it/350x200' alt="Card image cap"/>
               <div className="card-block">
@@ -24,6 +24,6 @@ const Resturants = ({restaurants}) => {
 
 Resturants.propType = {
   restaurants: PropTypes.array
-};
+}
 
 export default Resturants;
