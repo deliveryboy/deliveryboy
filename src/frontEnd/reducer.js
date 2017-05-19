2420f894da9c3bf011cade39a09b7dce31fb1594
abeeralshaer <abber2000g@gmail.com>
2017-05-19 12:49:58 -0500

    add reducers

--- src/frontEnd/reducer.js
+++ src/frontEnd/reducer.js
@@ -6,4 +6,76 @@ const currentView = (state='',action)=>{
     return state;
   }
 };
-export {currentView};
+
+const currentUser = (state = {},action)=>{
+  switch (action.payload) {
+  case 'UPDATE_PROFILE':
+    return action.payload;
+  default:
+    return state;
+  }
+};
+
+const restaurants = (state = [],action)=>{
+  switch (action.payload) {
+  case 'GET_RESTAURANTS':
+    return action.payload;
+  default:
+    return state;
+  }
+};
+
+const currentRestaurant = (state = {},action)=>{
+  switch (action.payload) {
+  case 'GET_RESTAURANT':
+    return action.payload;
+  default:
+    return state;
+
+  }
+};
+
+const deliverboyList = (state={},action)=>{
+  switch (action.payload) {
+  case 'GET_DELIVERYBOYSLIST':
+    return action.payload;
+  default:
+    return state;
+  }
+};
+
+const deliverboy = (state={},action)=>{
+  switch (action.payload) {
+  case 'UPDATE_DELIVERYPROFILE':
+    return action.payload;
+  default:
+    return state;
+  }
+};
+
+const meals = (state=[],action)=>{
+  switch (action.payload) {
+  case 'GET_MEALS':
+    return action.payload;
+  default:
+    return state;
+  }
+
+};
+
+const ordersList = (state = [],action)=>{
+  switch (action.payload) {
+  case 'FETCH_ORDERS':
+    return action.payload;
+  default:
+    return state;
+
+  }
+};
+
+
+
+
+
+
+export {currentView,currentUser,restaurants,currentRestaurant,deliverboyList,deliverboy,meals,ordersList};
