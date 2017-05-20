e2c4473a811a403367570a9767e83109c70c4485
abeeralshaer <abber2000g@gmail.com>
2017-05-19 23:49:42 -0500

    change file name typo

---
+++ src/frontEnd/components/Restaurant.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Restaurants = ({restaurants}) => {
+  return (
+    <div className='row'>
+      {restaurants.map(function(restaurant) {
+        return (
+          <div className="col-xs-12 col-sm-6 col-md-6 float-card">
+            <div className="card">
+              <img className="card-img-top img-responsive" src='http://placehold.it/350x200' alt="Card image cap"/>
+              <div className="card-block">
+                <h4 className="card-title">زاد الخير</h4>
+                <p className="card-text">
+                  المكان: غزة</p>
+              </div>
+            </div>
+          </div>
+        );
+      })};
+    </div>
+  );
+};
+
+Restaurants.propType = {
+  restaurants: PropTypes.array
+}
+
+export default Restaurants;
