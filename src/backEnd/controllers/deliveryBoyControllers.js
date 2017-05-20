f5af7e804615b6cd8ee9d042faa3abee5b5d3382
shahenaz Monia <shahy.m.93@gmail.com>
2017-05-20 09:16:25 +0300

    add deliveryorder fun

--- src/backEnd/controllers/deliveryBoyControllers.js
+++ src/backEnd/controllers/deliveryBoyControllers.js
@@ -1,4 +1,5 @@
 const deliveryboydb = require('../../database/deliveryboy.js');
+const orderdb = require('../../database/order.js');
 const path= require('path');
 
 module.exports = {
@@ -17,6 +18,13 @@ module.exports = {
       else {
         res.json(result);
       }
+    });
+  },
+  getDeliveryboyOrder: (req, res) => {
+    ///selectresstatus if status == accept
+    orderdb.selectOrderBYDeliverytStatus('pending',(err,pendingOrders)=>{
+      console.log(pendingOrders);
+      res.json(pendingOrders);
     });
   }
 };
